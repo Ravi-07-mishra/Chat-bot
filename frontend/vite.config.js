7d9932a6a4265f580c0b0ada871d72ac5056622f
Ravi Mishra <ravi19mishra10@gmail.com>
2025-06-19 20:03:18 +0530

    vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // ✅ important for relative paths on Vercel
   plugins: [react()],
   server: {
     proxy: {
